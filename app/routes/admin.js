9d0ffe2b19b63a515a4f91a54f40743d0b97c149
Arnaud Tricoire <arnaud.tricoire@hotmail.com>
2025-02-06 14:29:29 +0100

    fix routes

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -10,10 +10,10 @@ const movieController = require('../controllers/movieController');
 const categoryController = require('../controllers/categoryController');
 
 // Routes du tableau de bord admin
-router.get('/dashboard', adminMiddleware, adminController.getDashboard);
+router.get('/tableau-de-bord', adminMiddleware, adminController.getDashboard);
 
 // Routes de gestion des utilisateurs
-router.get('/users', adminMiddleware, adminController.getAllUsers);
+router.get('/utilisateur', adminMiddleware, adminController.getAllUsers);
 router.put('/users/:id', adminMiddleware, adminController.updateUser);
 router.delete('/users/:id', adminMiddleware, adminController.deleteUser);
 
@@ -28,18 +28,17 @@ router.put('/utensils/:id', adminMiddleware, utensilController.updateUtensil);
 router.delete('/utensils/:id', adminMiddleware, utensilController.deleteUtensil);
 
 // Routes de gestion des recettes
+router.get('/recette', adminMiddleware, adminController.getRecipes);
 router.post('/recipes', adminMiddleware, recipeController.createRecipe);
 router.put('/recipes/:id', adminMiddleware, recipeController.updateRecipe);
 router.delete('/recipes/:id', adminMiddleware, recipeController.deleteRecipe);
 
 // Routes de gestion des films/séries
+router.get('/films-series', adminMiddleware, adminController.showaddmoviesTvShows);
+router.post('/films-series', adminMiddleware, adminController.addmoviesTvShows);
 router.post('/movies', adminMiddleware, movieController.createMovie);
 router.put('/movies/:id', adminMiddleware, movieController.updateMovie);
 router.delete('/movies/:id', adminMiddleware, movieController.deleteMovie);
-
-// Routes pour afficher les pages d'administration
-router.get('/recipes', adminMiddleware, adminController.getRecipes);
-router.get('/users', adminMiddleware, adminController.getAllUsers);
 
 // Routes pour la gestion des catégories
 router.get('/categories', adminMiddleware, categoryController.getAllCategories);
